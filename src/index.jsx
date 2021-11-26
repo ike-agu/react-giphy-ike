ce93e9813a67d22bcc68d27a389e48777ce1649c
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-11-26 15:07:06 +0000

    added search bar

--- src/index.jsx
+++ src/index.jsx
@@ -2,49 +2,14 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-
-// const Hello = ({ name }) => {
-//   return (
-//     <div>
-//       My name is iyke strike , Hello 
-//       {name}
-//     </div>
-//   );
-// };
-
-class Hello extends Component {
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      clicked: false
-
-    };
- 
-  }
-
-  handleClick = () => {
-     this.setState({
-       clicked: !this.state.clicked
-     });
-
-  }
-
-  render () {
-    return (
-      <div className = {this.state.clicked ? 'clicked' : null }
-      onClick = {this.handleClick}>
-        Hello {this.props.name}
-      </div>
-    )
-
-  }
+import App from './components/app.jsx';
 
 
-}
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<App />, root);
 }
+ 
+
  
