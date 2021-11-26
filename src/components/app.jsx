0d8ba9fa9564dceae50a710c9c3a3da1d7fefcd2
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-11-26 20:24:15 +0000

    fetching data from giphy-api

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
 
-class App extends Component {
 
+class App extends Component {
+  
     constructor (props) {
         super(props);
         this.state = {
           gifs: [],
           selectedGifId: "pVkmGyqYRt4qY" 
         }
+
+        this.search("nba");
     }
 
-    // TODO: Call API
+    // TODO: Call Giphy API
 
     search = (query) => {
-        
+        giphy('5ziKr3tUHkbRAuDqSM14Rj5639LA6H4l').search({
+            q: query,
+            rating: 'g',
+            limit: 15
+        }, (error, result) => {
+            this.setState({
+                gifs: result.data
+            })
+        });
+
     }
 
 
     render() {
         
-        return (
+        return ( 
             <div> 
                 <div className="left-scene">
                     <SearchBar />
