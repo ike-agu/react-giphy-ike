902422d51cc77cb155af2170bdcdab987aae4cc6
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-11-26 18:16:41 +0000

    added an array of gifs and mapped each one

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,22 +2,32 @@ import React, { Component } from 'react';
 
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
     render() {
+        const gifs = [
+            {id:"pVkmGyqYRt4qY"},
+            {id: "QvwMDYpAMUm6Q"},
+            {id: "cuPm4p4pClZVC"},
+            {id: "8PsTsGXP1QB3LihxTB"}
+    
+        ];
         return (
-            <div>
+            <div> 
                 <div className="left-scene">
                     <SearchBar />
                     <div className="selected-gif">
                         <Gif  id = "pVkmGyqYRt4qY" />
                     </div>
                 </div>
-                <div className="right-scene"></div>
+                <div className="right-scene">
+                    <GifList gifs = {gifs}/>
+                </div>
             </div>
         );
 
     }
 }
 
-export default App;
+export default App; 
